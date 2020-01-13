bbdfa6d7b66440eb9095e688e8da9b1d52b80a0f
Michelle Morin and Patrick Kille <michelle@>
2020-01-13 10:02:39 -0800

    add additional functions to back-end logic and link stylesheets to HTML page

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,55 @@
-var cup = function(tbs) {
+var tbsToCup = function(tbs) {
 	return tbs / 16;
-}
+};
 
-var tbs = function(tsp) {
+var cupToTbs = function(cup) {
+	return cup * 16;
+};
+
+var tspToTbs = function(tsp) {
 	return tsp / 3;
-}
+};
 
-var gal = function(cup) {
+var tbsToTsp = function(tbs) {
+	return tbs * 3;
+};
+
+var cupToGal = function(cup) {
 	return cup / 16;
-}
+};
 
-var gal = function(qt) {
+var galToCup = function(gal) {
+	return gal * 16;
+};
+
+var qtToGal = function(qt) {
 	return qt /4;
-}
+};
 
-var qt = function(pt) {
+var galToQt = function(gal) {
+	return gal * 4;
+};
+
+var ptToQt = function(pt) {
 	return pt / 2;
-}
+};
 
-var pt = function(cup) {
+var qtToPt = function(qt) {
+	return qt * 2;
+};
+
+var cupToPt = function(cup) {
 	return cup / 2;
-}
+};
 
-var floz = function(cup) {
+var ptToCup = function(pt) {
+	return pt * 2;
+};
+
+var cupToFlOz = function(cup) {
 	return cup / 8;
-}
+};
+
+var flOzToCup = function(flOz) {
+	return flOz * 8;
+};
