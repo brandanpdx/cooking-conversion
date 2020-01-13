691b1d0c9e6c26bb8585ac36619ada4e53c7e07f
Michelle Morin and Patrick Kille <michelle@>
2020-01-13 09:52:50 -0800

    add README and add business logic to scripts file

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var cup = function(tbs) {
+	return tbs / 16;
+}
+
+var tbs = function(tsp) {
+	return tsp / 3;
+}
+
+var gal = function(cup) {
+	return cup / 16;
+}
+
+var gal = function(qt) {
+	return qt /4;
+}
+
+var qt = function(pt) {
+	return pt / 2;
+}
+
+var pt = function(cup) {
+	return cup / 2;
+}
+
+var floz = function(cup) {
+	return cup / 8;
+}
